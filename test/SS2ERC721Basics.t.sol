d37fa424de547283f35940de6a8662f8213a06c5
karmacoma <karma@coma.lol>
2023-03-08 16:06:01 -0800

    Update SS2ERC721Basics with best-case transferFrom test
    We already have a test for the worst case (primary owner to fresh secondary owner)
    Let's add another test for the best case (secondary owner to existing secondary owner):
    
         test_transferFrom_subsequent() (gas: -21900 (-32.614%))
    
    The batch tests have increased costs, presumably because we ship more data to the contract rather than just passing a pointer:
    
         test_mint_newcomer_0001() (gas: 434 (0.640%))
         test_mint_newcomer_0010() (gas: 816 (0.659%))
         test_mint_newcomer_0100() (gas: 4485 (0.657%))
         test_mint_newcomer_1000() (gas: 48045 (0.765%))
    
    Seems more realistic to test this way regardless

--- test/SS2ERC721Basics.t.sol
+++ test/SS2ERC721Basics.t.sol
@@ -13,8 +13,33 @@ contract BasicSS2ERC721 is SS2ERC721 {
         _mint(ptr);
     }
 
+    function mint(bytes calldata recipients) public {
+        _mint(SSTORE2.write(recipients));
+    }
+
     function tokenURI(uint256) public view virtual override returns (string memory) {
         return "";
+    }
+}
+
+/// @dev code from nft-editions/utils/Addresses.sol
+function make(uint256 n) pure returns (bytes memory addresses) {
+    assembly {
+        addresses := mload(0x40)
+        let data := add(addresses, 32)
+
+        // start with the dolphin address
+        let addr := 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
+        for { let i := n } gt(i, 0) { i := sub(i, 1) } { mstore(add(data, sub(mul(i, 20), 32)), add(addr, i)) }
+
+        let last := add(data, mul(n, 20))
+
+        // store the length
+        mstore(addresses, mul(n, 20))
+
+        // Allocate memory for the length and the bytes,
+        // rounded up to a multiple of 32.
+        mstore(0x40, and(add(last, 31), not(31)))
     }
 }
 
@@ -27,54 +52,32 @@ contract BasicERC721Test is Test {
 
     address private alice = makeAddr("alice");
     address private bob = makeAddr("bob");
+    address private carlotta = makeAddr("carlotta");
 
     function setUp() public {
         nftContract = new BasicSS2ERC721("basic", unicode"✌️");
         nftContract_preminted = new BasicSS2ERC721("preminted", unicode"✌️");
         nftContract_pretransferred = new BasicSS2ERC721("pretransferred", unicode"✌️");
 
-        address ptr = SSTORE2.write(abi.encodePacked(alice));
-
         // alice starts with a token
-        nftContract_preminted.mint(ptr);
+        nftContract_preminted.mint(SSTORE2.write(abi.encodePacked(alice)));
 
         // set up the pre-transfer
-        nftContract_pretransferred.mint(ptr);
+        nftContract_pretransferred.mint(SSTORE2.write(abi.encodePacked(bob, alice)));
 
         vm.prank(alice);
-        nftContract_pretransferred.transferFrom(alice, bob, 1);
+        nftContract_pretransferred.transferFrom(alice, bob, 2);
 
         vm.prank(bob);
-        nftContract_pretransferred.transferFrom(bob, alice, 1);
+        nftContract_pretransferred.transferFrom(bob, carlotta, 1);
 
         // set up the 1000 token test
         ptr1000 = SSTORE2.write(make(1000));
     }
 
-    /// @dev code from nft-editions/utils/Addresses.sol
-    function make(uint256 n) private pure returns (bytes memory addresses) {
-        assembly {
-            addresses := mload(0x40)
-            let data := add(addresses, 32)
-
-            let addr := 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
-            for { let i := n } gt(i, 0) { i := sub(i, 1) } { mstore(add(data, sub(mul(i, 20), 32)), add(addr, i)) }
-
-            let last := add(data, mul(n, 20))
-
-            // store the length
-            mstore(addresses, mul(n, 20))
-
-            // Allocate memory for the length and the bytes,
-            // rounded up to a multiple of 32.
-            mstore(0x40, and(add(last, 31), not(31)))
-        }
-    }
-
     function mint(uint256 n) private {
         bytes memory addresses = make(n);
-        address ptr = SSTORE2.write(addresses);
-        nftContract.mint(ptr);
+        nftContract.mint(addresses);
     }
 
     function test_mint_newcomer_0001() public {
@@ -103,8 +106,8 @@ contract BasicERC721Test is Test {
     }
 
     function test_transferFrom_subsequent() public {
-        vm.prank(alice);
-        nftContract_pretransferred.transferFrom(alice, bob, 1);
+        vm.prank(carlotta);
+        nftContract_pretransferred.transferFrom(carlotta, bob, 1);
     }
 
     function test_balanceOf() public view {
