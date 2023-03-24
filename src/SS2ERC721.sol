0883c1a48e17a85aa65725ef68303ac17c427209
codeislight1 <codeislight@gmail.com>
2023-03-25 06:51:52 +0800

    Revert "Unchecked operations ⚡"
    This reverts commit 38e6534ad05635b54a00717b00d017b9682818c2.

--- src/SS2ERC721.sol
+++ src/SS2ERC721.sol
@@ -133,20 +133,19 @@ abstract contract SS2ERC721 is ERC721 {
     function _balanceOfPrimary(address owner) internal view returns (uint256) {
         uint256 low = 1;
         uint256 high = _ownersPrimaryLength();
-        unchecked {
-            uint256 mid = (low + high) / 2;
+        uint256 mid = (low + high) / 2;
 
-            while (low <= high) {
-                address midOwner = _ownerOfPrimary(mid);
-                if (midOwner == owner) {
-                    return 1;
-                } else if (midOwner < owner) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-                mid = (low + high) / 2;
+        // TODO: unchecked
+        while (low <= high) {
+            address midOwner = _ownerOfPrimary(mid);
+            if (midOwner == owner) {
+                return 1;
+            } else if (midOwner < owner) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
+            mid = (low + high) / 2;
         }
 
         return 0;
@@ -175,9 +174,7 @@ abstract contract SS2ERC721 is ERC721 {
         balance = balanceIndicator & BALANCE_MASK;
 
         if (balanceIndicator & SKIP_PRIMARY_BALANCE == 0) {
-            unchecked {
-                balance += _balanceOfPrimary(owner);
-            }
+            balance += _balanceOfPrimary(owner);
         }
     }
 
