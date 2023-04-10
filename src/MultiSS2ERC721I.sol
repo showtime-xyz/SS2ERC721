f01cd778c065f6b7b64776e9cd027c6a070cede0
karmacoma <karma@coma.lol>
2023-04-10 09:42:58 -0700

    Add MultiSS2ERC721I
    + move more files into common/

--- src/MultiSS2ERC721I.sol
+++ src/MultiSS2ERC721I.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import {SS2ERC721} from "./SS2ERC721.sol";
+import {MultiSS2ERC721} from "./MultiSS2ERC721.sol";
 import {Initializable} from "./common/utils/Initializable.sol";
 
-/// @notice Initializable version of SS2ERC721
-abstract contract SS2ERC721I is SS2ERC721, Initializable {
+/// @notice Initializable version of MultiSS2ERC721
+abstract contract MultiSS2ERC721I is MultiSS2ERC721, Initializable {
     /*//////////////////////////////////////////////////////////////
                               CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
-    constructor() SS2ERC721("", "") {
+    constructor() MultiSS2ERC721("", "") {
         _lockInitializers();
     }
 
@@ -18,7 +18,7 @@ abstract contract SS2ERC721I is SS2ERC721, Initializable {
                               INITIALIZER
     //////////////////////////////////////////////////////////////*/
 
-    function __SS2ERC721_init(string memory _name, string memory _symbol) internal onlyInitializing {
+    function Multi__SS2ERC721_init(string memory _name, string memory _symbol) internal onlyInitializing {
         name = _name;
         symbol = _symbol;
     }
